fecf40dee4357a9ca9767f98fdbc8a08803502b3
Gautham <korrapatigautham@gmail.com>
2026-01-17 19:58:17 -0800

    smaller model

--- video/reason.py
+++ video/reason.py
@@ -1,49 +1,37 @@
-from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from transformers import AutoProcessor, AutoModelForImageTextToText
 import torch
-from qwen_vl_utils import process_vision_info
 
-model = Qwen2VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2-VL-7B-Instruct", 
-    torch_dtype="auto", 
-    device_map="auto",
-    attn_implementation="flash_attention_2"    
-)
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct")
+processor = AutoProcessor.from_pretrained("HuggingFaceTB/SmolVLM-Instruct")
+model = AutoModelForImageTextToText.from_pretrained(
+    "HuggingFaceTB/SmolVLM-Instruct",
+    torch_dtype = torch.bfloat16,
+    _attn_implmentation="flash_attention_2" if device == "cuda" else "eager"
+).to(device)
 
 messages = [
     {
         "role": "user",
         "content": [
-            {
-                "type": "image",
-                "image": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg",
-            },
-            {"type": "text", "text": "Describe this image."},
+            {"type": "video", "path": "./data/body_cam_model_test.mp4"},
+            {"type": "text", "text": "Describe this video."},
         ],
     }
 ]
 
-text = processor.apply_chat_template(
-    messages, tokenize=False, add_generation_prompt=True
-)
-
-image_inputs, video_inputs = process_vision_info(messages)
-inputs = processor(
-    text=[text],
-    images=image_inputs,
-    videos=video_inputs,
-    padding=True,
+inputs = processor.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    tokenize=True,
+    return_dict=True,
     return_tensors="pt"
 )
 
-# inputs = inputs.to("cuda")
-generated_ids = model.generate(**inputs, max_new_tokens=128)
-generated_ids_trimmed = [
-    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-]
-output_text = processor.batch_decode(
-    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+generated_ids = model.generate(**inputs, do_sample=False, max_new_tokens=64)
+generated_texts = processor.batch_decode(
+    generated_ids,
+    skip_special_tokens=True
 )
 
-print(output_text)
+print(generated_texts[0])
